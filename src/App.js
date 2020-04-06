570c33355163e02c029253b6389749f1571fb6f8
kc15781 <kc15781@my.bristol.ac.uk>
2020-04-06 14:30:22 +0800

    miscellaneous updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { IntlProvider } from "react-intl";
 import {Bottom_component} from './components/shared_components/bottom.component.js'
 import { NavBar } from "./components/shared_components/navbar.component"
 import Home from "./components/Home";
+import Error from "./components/404";
 import Product from "./components/Product";
 import Employment from "./components/Employment";
 import Contact from "./components/Contact";
@@ -28,12 +29,12 @@ function App() {
 
                 <NavBar change_locale={change_locale} />
  
-           
+
                 <Route path="/" exact render={(props) => <Home {...props} locale={locale} />} />
                 <Route path="/Product" component={Product} />
                 <Route path="/Employment" component={Employment} />
                 <Route path="/Contact" render={(props) => <Contact {...props} locale={locale} />} />
-                
+
                 <div className="bg_color2 pt-4 mt-5">
                 <Container className="">
                 <Bottom_component />
